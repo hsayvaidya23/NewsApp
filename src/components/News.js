40ff2c16028688911d6fdb26f24be27bc3f92995
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-02-08 18:12:32 +0530

    video 24 react newsapp

--- src/components/News.js
+++ src/components/News.js
@@ -4,15 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-        <div>
-            This is a news component.
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
-            
+        <div className="container my-3">
+            <h2>NewsMonkey - Top Headlines</h2>
+            <div className="row">
+                <div className="col-md-4">
+                    <NewsItem title = "myTitle" description="myDesc"/>
+                </div>
+                <div className="col-md-4">
+                    <NewsItem title = "myTitle" description="myDesc"/>  
+                </div>
+                <div className="col-md-4">
+                    <NewsItem title = "myTitle" description="myDesc"/>
+                </div>   
+            </div>  
         </div>
         )
     }
