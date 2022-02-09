300d697b9b7b47e51b24d320bbc8bea67fff5290
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-02-09 16:07:55 +0530

    video  27 react newsapp

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,9 +6,9 @@ export class NewsItem extends Component {
         return (
         <div className="my-3">
             <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-89447573,width-1070,height-580,overlay-toi_sw,pt-32,y_pad-40,resizemode-75,imgsize-56260/89447573.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title"> {title}...</h5>
+                    <h5 className="card-title"> {title}</h5>
                     <p className="card-text">{description} </p>
                     <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
                 </div>
