5e553d476467dab3b54fddbe0e421ef975791ffd
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-02-11 15:57:49 +0530

    video 32 react newsapp

--- src/components/News.js
+++ src/components/News.js
@@ -76,8 +76,8 @@ export class News extends Component {
             {!this.state.loading && this.state.articles.map((element)=>{ 
               return <div className="col-md-4" key={element.url}>
                     <NewsItem title={element.title?element.title: ""} description={element.description?element.description:""} 
-                    imageUrl={element.urlToImage} newsUrl = {element.url}
-                    />
+                    imageUrl={element.urlToImage} newsUrl = {element.url} author={element.author} date={element.publishedAt}
+                    source={element.source.name}    />
                 </div>
 
             })};
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description, imageUrl,newsUrl} = this.props;
+        let {title, description, imageUrl,newsUrl,author,date,source} = this.props;
         return (
         <div className="my-3">
             <div className="card">
+                <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}} >{source} </span>
                 <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-89447573,width-1070,height-580,overlay-toi_sw,pt-32,y_pad-40,resizemode-75,imgsize-56260/89447573.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title"> {title}</h5>
+                    <h5 className="card-title"> {title} </h5>
                     <p className="card-text">{description} </p>
+                    <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()} </small></p>
                     <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
