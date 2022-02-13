752a9b1a0d2fa02d3cdf0649a1efe80d98e3fc46
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-02-13 13:28:37 +0530

    video 34 react newsapp

--- src/components/News.js
+++ src/components/News.js
@@ -15,13 +15,17 @@ export class News extends Component {
         pageSize:PropTypes.number,
         category:PropTypes.string
     }
-    constructor() {
-        super();
+    capitalizeFirstLetter = (string) => {
+        return  string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    constructor(props) {
+        super(props);
         this.state= {
             articles:[],
             loading: false, 
             page: 1
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`
     }
     async updateNews(){
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=857c2f8193ac4125aa7ae4f9129e1ccc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
@@ -50,7 +54,7 @@ export class News extends Component {
     render() {
         return (
         <div className="container my-3">
-            <h1 className="text-center" style={{margin:'35px 0px'}} >NewsMonkey - Top Headlines</h1>
+            <h1 className="text-center" style={{margin:'35px 0px'}} >NewsMonkey - Top  {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
             {this.state.loading && <Spinner/>}
             <div className="row">
             {!this.state.loading && this.state.articles.map((element)=>{ 
